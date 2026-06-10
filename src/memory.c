8b09bbb446b9445c854820581d41b7ca7d7ecfc1
luis Ortega <ortegaluiskb@gmail.com>
2026-06-10 16:05:54 -0600

    Create memory

--- src/memory.c
+++ src/memory.c
@@ -6,5 +6,9 @@ int main(void) {
     int stack_one = 24;
     printf("Stack : %d\n", stack_one);
     printf("Firts stack in memory: %p\n", &stack_one);
+
+    int *stack_two = &stack_one;
+    printf("%p\n", stack_two);
+    printf("%p\n", (void *)stack_two);
     return 0;
 }
