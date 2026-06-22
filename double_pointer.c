eb09422e72a56e8c15946088104ddd52ebb07113
luisOrtga <ortegaluiskb@gmail.com>
2026-06-21 23:10:26 -0600

    Create double pointer

---
+++ double_pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void cambiarPuntero(int **pp) {
+    **pp = 100;  // Modifica el valor al que apunta el puntero
+}
+
+int main() {
+    int x = 50;
+    int *p = &x;        // p apunta a x
+    int **pp = &p;      // pp apunta a p
+    
+    printf("x antes: %d\n", x);  // 50
+    cambiarPuntero(pp);
+    printf("x después: %d\n", x);  // 100
+    
+    return 0;
+}
